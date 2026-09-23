875059e09345d8f9eb9da95a885d9feb8ed87f57
Passass <you@example.com>
2026-09-23 18:35:20 +1000

    support for 4.1.6

--- test.cs
+++ test.cs
@@ -1,42 +1,47 @@
-﻿using System.Reflection;
-using SPTarkov.DI.Annotations;
+﻿using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Models.Spt.Mod;
+using System.Reflection;
+using WTTServerCommonLib;
 using Range = SemanticVersioning.Range;
 
 namespace TEST;
 
-public record ModMetadata : AbstractModMetadata
+public record ModMetadata : IModMetadata
 {
-    public override string ModGuid { get; init; } = "com.test.test";
-    public override string Name { get; init; } = "Test";
-    public override string Author { get; init; } = "Test";
-    public override List<string>? Contributors { get; init; } = ["Test"];
-    public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
-    public override Range SptVersion { get; init; } = new("~4.0.13");
-    public override string? Url { get; init; } = "";
-    public override List<string>? Incompatibilities { get; init; }
-    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    public string ModGuid { get; init; } = "com.test.test";
+    public string Name { get; init; } = "Test";
+    public string Author { get; init; } = "Test";
+    public List<string>? Contributors { get; init; } = ["Test"];
+    public SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
+    public Range SptVersion { get; init; } = new("~4.1.6");
+    public string? Url { get; init; } = "";
+    public List<string>? Incompatibilities { get; init; }
+    public Dictionary<string, Range>? ModDependencies { get; init; } = new()
     {
-        { "com.wtt.commonlib", new Range("~2.0.20") },
+        { "com.wtt.commonlib", new Range("~3.0.6") },
     };
-    public override bool? IsBundleMod { get; init; } = false;
-    public override string License { get; init; } = "MIT";
+    public bool? IsBundleMod { get; init; } = false;
+    public string? License { get; init; } = "MIT";
+    public bool HasPrepatcher { get; init; } = false;
 }
 
-[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 4)]
-public class SOCOM(
+[Injectable(TypePriority = OnLoadOrder.Preload + 4)]
+public class Test(
     WTTServerCommonLib.WTTServerCommonLib wttCommon) : IOnLoad
 {
-    public async Task OnLoad()
+    public Task OnLoadAsync(CancellationToken cancellationToken)
     {
         Assembly assembly = Assembly.GetExecutingAssembly();
-        await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
-        await wttCommon.CustomQuestService.CreateCustomQuests(assembly);
-        await wttCommon.CustomLootspawnService.CreateCustomLootSpawns(assembly);
-        await wttCommon.CustomLocaleService.CreateCustomLocales(assembly);
-        await wttCommon.CustomQuestZoneService.CreateCustomQuestZones(assembly);
-        await wttCommon.CustomAssortSchemeService.CreateCustomAssortSchemes(assembly);
-        await wttCommon.CustomBuffService.CreateCustomBuffs(assembly);
+        wttCommon.CustomQuestItemService.CreateCustomQuestItems(assembly);
+        wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
+        wttCommon.CustomQuestService.CreateCustomQuests(assembly);
+        wttCommon.CustomLootspawnService.CreateCustomLootSpawns(assembly);
+        wttCommon.CustomLocaleService.CreateCustomLocales(assembly);
+        wttCommon.CustomQuestZoneService.CreateCustomQuestZones(assembly);
+        wttCommon.CustomAssortSchemeService.CreateCustomAssortSchemes(assembly);
+        wttCommon.CustomBuffService.CreateCustomBuffs(assembly);
+
+        return Task.CompletedTask;
     }
 }
